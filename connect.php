50c33005da93a7a16057466ec6a197d185266e3e
Baoy <Baoy@users.noreply.github.com>
2014-04-18 00:59:18 -0500

    Ejemplo de select
    SELECT * FROM DOCTOR, explicado mas o menos.

--- connect.php
+++ connect.php
@@ -2,6 +2,7 @@
 
 <?php
 // Create connection to Oracle
+//la coneccion obviamente
 
 $MYDB ="
      (DESCRIPTION =
@@ -14,13 +15,40 @@ $MYDB ="
 	
 $conn = oci_connect("a01226103", "14db103", $MYDB);
 if (!$conn) {
-   $m = oci_error();
-   echo $m['message'], "\n";
-   exit;
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-else {
-   print "Connected to Oracle!";
+
+// Prepare the statement
+//El querie tal como lo usarias en el DBM, parse lo prepara, recive la coneccion y el string
+$stid = oci_parse($conn, 'SELECT * FROM doctor');
+if (!$stid) {
+    $e = oci_error($conn);
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-// Close the Oracle connection
+
+// Perform the logic of the query
+// Ejecuta el querie
+$r = oci_execute($stid);
+if (!$r) {
+    $e = oci_error($stid);
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+// Fetch the results of the query
+//Toma los datos, revisa y mientras alla una fila crea una para la tabla. El foreach recorre las columnas que regresa el resultado
+print "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    print "<tr>\n";
+    foreach ($row as $item) {
+        print "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    print "</tr>\n";
+}
+print "</table>\n";
+
+//cerrar conexion
+oci_free_statement($stid);
 oci_close($conn);
+
 ?>
